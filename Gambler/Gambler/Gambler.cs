e1db85281b1e9c4c9a158fe055346e4c98fcb176
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-10-06 10:34:27 +0530

    Added: Glamber Problem

---
+++ Gambler/Gambler/Gambler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Gambler
+{
+    public class Gambler
+    {
+        int player1;
+        int Everyday = 100;
+        int EachGame = 1;
+    }
+}
---
+++ Gambler/Gambler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gambler Problem");
+            
+        }
+    }
+}
